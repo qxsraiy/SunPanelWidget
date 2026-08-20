aed2e681f1cac6ca6317873e82c53f03e9415e21
qxsraiy <qxsraiy@gmail.com>
2026-08-20 19:44:18 +0800

    面板管理完成

--- app/src/main/java/com/sunpanel/widget/SunPanelWidgetProvider.kt
+++ app/src/main/java/com/sunpanel/widget/SunPanelWidgetProvider.kt
@@ -8,6 +8,7 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
+import android.graphics.Color
 import android.util.Log
 import android.view.View
 import android.widget.RemoteViews
